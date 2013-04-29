d39dab079448d08f47d6561829df03605201366b
Andrew Burke <donandres@yahoo.com>
2013-04-29 15:05:56 -0400

    4_29_initialFinalProduct

---
+++ HTMLLearnerLessonObject.h
@@ -0,0 +1,13 @@
+//
+//  HTMLLearnerLessonObject.h
+//  HTMLLearner
+//
+//  Created by Andrew Burke on 4/25/13.
+//  Copyright (c) 2013 CS639. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface HTMLLearnerLessonObject : NSObject
+
+@end
---
+++ HTMLLearnerLessonObject.m
@@ -0,0 +1,13 @@
+//
+//  HTMLLearnerLessonObject.m
+//  HTMLLearner
+//
+//  Created by Andrew Burke on 4/25/13.
+//  Copyright (c) 2013 CS639. All rights reserved.
+//
+
+#import "HTMLLearnerLessonObject.h"
+
+@implementation HTMLLearnerLessonObject
+
+@end
